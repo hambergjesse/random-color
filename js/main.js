da017fd724ad95b42868e91552493f93f5241c07
hambergjesse <hambergjesse@gmail.com>
2022-05-19 14:04:09 +0300

    get rid of pointless hsl code

--- js/main.js
+++ js/main.js
@@ -23,23 +23,6 @@ function generateColor(r, g, b, a) {
   document.getElementById("main-sub-canvas").style.background =
     "rgba(" + r + ", " + g + ", " + b + ", " + a + ")";
 
-  const RGBToHSL = (r, g, b) => {
-    const l = Math.max(r, g, b);
-    const s = l - Math.min(r, g, b);
-    const h = s
-      ? l === r
-        ? (g - b) / s
-        : l === g
-        ? 2 + (b - r) / s
-        : 4 + (r - g) / s
-      : 0;
-    return (hslStr = [
-      60 * h < 0 ? 60 * h + 360 : 60 * h,
-      100 * (s ? (l <= 0.5 ? s / (2 * l - s) : s / (2 - (2 * l - s))) : 0),
-      (100 * (2 * l - s)) / 2,
-    ]);
-  };
-
   // display RGBA values as text
   const rgbaStr = "rgba(" + r + ", " + g + ", " + b + ", " + a + ")";
   document.getElementById("main-rgba-value").innerText = rgbaStr;
